2a923a16874f19c4a6a67cde33ec0b5566dfa3c8
ECNUJason <1123781124@qq.com>
2017-12-17 22:27:27 +0800

    增加了一个用户的自我描述的板块

--- src/com/cwj/taiqiangle/controller/UserManageController.java
+++ src/com/cwj/taiqiangle/controller/UserManageController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/user")
 public class UserManageController {
-    UserService userService=new UserService();
-    SendMailService sendMailService=new SendMailService();
+    UserService userService = new UserService();
+    SendMailService sendMailService = new SendMailService();
 
 
     /**
@@ -24,35 +25,29 @@ public class UserManageController {
      * Code:206 Data=-3 用户密码错误
      * Code:207 Data=-2 用户账户被重复
      * Code:404 Data=-1 catch异常
+     *
      * @param username
      * @param password
      * @return
      */
     @RequestMapping(value = "/userLogin", method = RequestMethod.GET)
     @ResponseBody
-    public JsonMsg userLogin(String username,String password)
-    {
-        JsonMsg jsonMsg=new JsonMsg();
+    public JsonMsg userLogin(String username, String password) {
+        JsonMsg jsonMsg = new JsonMsg();
         try {
-            if(userService.getUserByName(username).size()==0)
-            {
+            if (userService.getUserByName(username).size() == 0) {
                 jsonMsg.setCode("205");
                 jsonMsg.setData(0);
-            }
-            else if (userService.getUserByName(username).size()!=1)
-            {
+            } else if (userService.getUserByName(username).size() != 1) {
                 jsonMsg.setCode("207");
                 jsonMsg.setData(-2);
-            }
-            else{
+            } else {
                 String id;
-                if(!userService.getUserByName(username).get(0).getPassword().equals(password))
-                {
+                if (!userService.getUserByName(username).get(0).getPassword().equals(password)) {
                     jsonMsg.setCode("206");
                     jsonMsg.setData(-3);
-                }
-                else{
-                    id=userService.getUserByName(username).get(0).getId();
+                } else {
+                    id = userService.getUserByName(username).get(0).getId();
                     jsonMsg.setCode("200");
                     jsonMsg.setData(userService.getUserById(id));
                 }
@@ -70,6 +65,7 @@ public class UserManageController {
      * Code="200"，Data=1（int）则注册成功
      * Code="202"，Data=0 (int)注册失败，用户已经存在
      * Code="404"  Data=-1页面丢失
+     *
      * @param username
      * @param password
      * @param email
@@ -78,25 +74,21 @@ public class UserManageController {
 
     @RequestMapping(value = "/userRegister", method = RequestMethod.GET)
     @ResponseBody
-    public JsonMsg addUser(String username,String password,String email)
-    {
-        JsonMsg jsonMsg=new JsonMsg();
-        try{
-            int result=
-            userService.addUser(username,password,email,null,null,0);
-            if(result==0)
-            {
+    public JsonMsg addUser(String username, String password, String email) {
+        JsonMsg jsonMsg = new JsonMsg();
+        try {
+            int result =
+                    userService.addUser(username, password, email, null, null, 0);
+            if (result == 0) {
                 jsonMsg.setData(0);
                 jsonMsg.setCode("202");
-            }
-            else{
+            } else {
                 jsonMsg.setData(1);
                 jsonMsg.setCode("200");
             }
 
 
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             jsonMsg.setData(-1);
             jsonMsg.setCode("404");
             e.printStackTrace();
@@ -110,23 +102,21 @@ public class UserManageController {
      * Code=200，Data=1，删除成功
      * Code=202，Data=0，删除失败
      * Code=404，Data=-1页面丢失
+     *
      * @param id
      * @return
      */
     @RequestMapping(value = "/userDelete", method = RequestMethod.GET)
     @ResponseBody
-    public JsonMsg removeUserById(String id)
-    {
-        JsonMsg jsonMsg=new JsonMsg();
+    public JsonMsg removeUserById(String id) {
+        JsonMsg jsonMsg = new JsonMsg();
         try {
 
-            int i=userService.removeUserById(id);
-            if(i==1)
-            {
+            int i = userService.removeUserById(id);
+            if (i == 1) {
                 jsonMsg.setCode("200");
                 jsonMsg.setData(1);
-            }
-            else{
+            } else {
                 jsonMsg.setCode("202");
                 jsonMsg.setData(0);
             }
@@ -143,6 +133,7 @@ public class UserManageController {
      * Code=200 Data=1发送成功
      * Code=205 Data=0发送失败
      * Code=404 Data=-1页面失效
+     *
      * @param email
      * @param username
      * @return
@@ -169,13 +160,14 @@ public class UserManageController {
     }
 
     /**
-     *Code=404 Data=-1 抛出异常不做改变
+     * Code=404 Data=-1 抛出异常不做改变
      * Code=202 Data=0 不变/修改无效
      * Code=202 Data=-2 id不存在
      * Code=202 Data=-3 名字修改时产生重复
      * Code=200 Data>=1 修改成功 Data=修改的信息数量
      * 参数都可以为null，但是id为null无法更新
      * null的参数不做更新
+     *
      * @param id
      * @param username
      * @param password
@@ -187,31 +179,26 @@ public class UserManageController {
      */
     @RequestMapping(value = "/userUpdate", method = RequestMethod.GET)
     @ResponseBody
-    public JsonMsg updateUser(String id,String username,String password,String email,String description,String pic,String money)
-    {
-        int status=0;
-        JsonMsg jsonMsg=new JsonMsg();
-        UserBean ub=new UserBean();
+    public JsonMsg updateUser(String id, String username, String password, String email, String description, String pic, String money) {
+        int status = 0;
+        JsonMsg jsonMsg = new JsonMsg();
+        UserBean ub = new UserBean();
         ub.setUserName(username);
         ub.setPassword(password);
         ub.setEmail(email);
         ub.setDescription(description);
         ub.setPic(pic);
-        try{
+        try {
             ub.setMoney(Integer.parseInt(money));
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             ub.setMoney(Integer.MIN_VALUE);
         }
         try {
-            status=userService.updateUserById(id,ub);
+            status = userService.updateUserById(id, ub);
             jsonMsg.setData(status);
-            if(status>=1)
-            {
+            if (status >= 1) {
                 jsonMsg.setCode("200");
-            }
-            else{
+            } else {
                 jsonMsg.setCode("202");
             }
         } catch (SQLException e) {
@@ -228,22 +215,20 @@ public class UserManageController {
      * Code:404,Data=-1查找失败
      * Code:200,Data=UserBean 查找成功
      * Code：202，Data=0，没有查到
+     *
      * @param id
      * @return
      */
     @RequestMapping(value = "/userLocate", method = RequestMethod.GET)
     @ResponseBody
-    public JsonMsg getUserByID(String id)
-    {
-        JsonMsg jsonMsg=new JsonMsg();
+    public JsonMsg getUserByID(String id) {
+        JsonMsg jsonMsg = new JsonMsg();
         try {
-            UserBean userBean=userService.getUserById(id);
-            if(userBean==null)
-            {
+            UserBean userBean = userService.getUserById(id);
+            if (userBean == null) {
                 jsonMsg.setData(0);
                 jsonMsg.setCode("202");
-            }
-            else{
+            } else {
                 jsonMsg.setData(userBean);
                 jsonMsg.setCode("200");
             }
@@ -258,15 +243,51 @@ public class UserManageController {
     }
 
     /**
+     * Code200，UserBean
+     * Code202,用户不存在
+     * Code404 用户被重复/
+     * Code404.Data -1 取出失败
+     * @return
+     */
+    @RequestMapping(value = "/getUserByName", method = RequestMethod.GET)
+    @ResponseBody
+    public JsonMsg getUsersByName(String username)
+    {
+        JsonMsg jsonMsg=new JsonMsg();
+        try {
+            List<UserBean> users= userService.getUserByName(username);
+            if(users.size()==0)
+            {
+                jsonMsg.setData(0);
+                jsonMsg.setCode("202");
+            }
+            else if(users.size()==1){
+                jsonMsg.setData(users.get(0));
+                jsonMsg.setCode("200");
+            }
+            else{
+                throw new SQLException("用户名被重复，sql出现问题");
+            }
+
+        } catch (SQLException e) {
+            jsonMsg.setCode("404");
+            jsonMsg.setData(-1);
+            e.printStackTrace();
+        }
+        return jsonMsg;
+    }
+
+
+    /**
      * Code200，Data List<UserBean>
      * Code404.Data -1 取出失败
+     *
      * @return
      */
     @RequestMapping(value = "/userTraversal", method = RequestMethod.GET)
     @ResponseBody
-    public JsonMsg getUsers()
-    {
-        JsonMsg jsonMsg=new JsonMsg();
+    public JsonMsg getUsers() {
+        JsonMsg jsonMsg = new JsonMsg();
         try {
             jsonMsg.setData(userService.getAllUserBean());
             jsonMsg.setCode("200");
