bfc55e978a189fac6c6df0260be47d62739485db
ECNUJason <1123781124@qq.com>
2017-12-17 11:37:46 +0800

    在项目里面不需要手动设置端口号码。我已经用JAVASCRIPT自动获取了。

--- web/js/logout.js
+++ web/js/logout.js
@@ -2,6 +2,17 @@ $(".logout").click(
     function() {
         $.cookie('username', null);
         alert("您已退出");
-        $(window).attr('location','http://localhost:8074/');
+        //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
+        var curWwwPath = window.document.location.href;
+        //获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
+        var pathName = window.document.location.pathname;
+        var pos;
+        if(pathName.length>1){pos = curWwwPath.indexOf(pathName);}
+        else{pos = -1;}
+        //获取主机地址，如： http://localhost:8083
+        var localhostPath;
+        if(pos>0){localhostPath = curWwwPath.substring(0, pos)+"/";}
+        else{localhostPath=curWwwPath;}
+        $(window).attr('location',localhostPath);
     }
 )
--- web/js/nav_user.js
+++ web/js/nav_user.js
@@ -5,59 +5,59 @@
  */
 /*这是全局的变量*/
 var navs = [{
-    "title" : "个人信息管理",
-    "icon" : "icon-computer",
-    "href" : "",
-    "spread" : false
-},{
-    "title":"租入订单",
-    "icon" : "icon-computer",
-    "href" : "page/main.html",
-    "spread" : false,
-    "children":[
+    "title": "个人信息管理",
+    "icon": "icon-computer",
+    "href": "page/user/userInfo.html",
+    "spread": false
+}, {
+    "title": "租入订单",
+    "icon": "icon-computer",
+    "href": "page/main.html",
+    "spread": false,
+    "children": [
         {
-            "title":"租入的车",
-            "icon" : "icon-computer",
-            "href" : "page/main.html",
-            "spread" : false,
-        },{
-            "title":"租入的车位",
-            "icon" : "icon-computer",
-            "href" : "page/main.html",
-            "spread" : false,
+            "title": "租入的车",
+            "icon": "icon-computer",
+            "href": "page/main.html",
+            "spread": false,
+        }, {
+            "title": "租入的车位",
+            "icon": "icon-computer",
+            "href": "page/main.html",
+            "spread": false,
         }
     ]
-},{
-    "title":"租出订单",
-    "icon" : "icon-computer",
-    "href" : "",
-    "spread" : false,
-    "children":[
+}, {
+    "title": "租出订单",
+    "icon": "icon-computer",
+    "href": "",
+    "spread": false,
+    "children": [
         {
-            "title":"租出订单",
-            "icon" : "icon-computer",
-            "href" : "",
-            "spread" : false,
+            "title": "租出订单",
+            "icon": "icon-computer",
+            "href": "",
+            "spread": false,
         }
     ]
-},{
-    "title":"发布车辆出租信息",
-    "icon" : "icon-computer",
-    "href" : "page/main.html",
-    "spread" : false
-},{
-    "title":"我要租车",
-    "icon" : "icon-computer",
-    "href" : "page/main.html",
-    "spread" : false
-},{
-    "title":"我要租车位",
-    "icon" : "icon-computer",
-    "href" : "page/main.html",
-    "spread" : false
-},{
-    "title":"留言系统",
-    "icon" : "icon-computer",
-    "href" : "page/main.html",
-    "spread" : false
+}, {
+    "title": "发布车辆出租信息",
+    "icon": "icon-computer",
+    "href": "page/main.html",
+    "spread": false
+}, {
+    "title": "我要租车",
+    "icon": "icon-computer",
+    "href": "page/main.html",
+    "spread": false
+}, {
+    "title": "我要租车位",
+    "icon": "icon-computer",
+    "href": "page/main.html",
+    "spread": false
+}, {
+    "title": "留言系统",
+    "icon": "icon-computer",
+    "href": "page/main.html",
+    "spread": false
 }]
